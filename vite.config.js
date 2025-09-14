d847dd45b7fb14f11dbfc5708542e005acf64493
Lida Guioma Echeverri <lecheverri24@estudiantes.areandina.edu.co>
2025-09-14 16:08:35 -0500

    vite.conf para produccion

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,4 @@
-import {
-    defineConfig
-} from 'vite';
+import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from "@tailwindcss/vite";
 
@@ -9,10 +7,32 @@ export default defineConfig({
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
+            // Agregado para producción:
+            buildDirectory: 'build', // asegura que vaya a public/build
         }),
         tailwindcss(),
     ],
     server: {
         cors: true,
     },
+    base: '/build/', // <-- importante para que Laravel encuentre los assets compilados
 });
+
+// import {
+//     defineConfig
+// } from 'vite';
+// import laravel from 'laravel-vite-plugin';
+// import tailwindcss from "@tailwindcss/vite";
+
+// export default defineConfig({
+//     plugins: [
+//         laravel({
+//             input: ['resources/css/app.css', 'resources/js/app.js'],
+//             refresh: true,
+//         }),
+//         tailwindcss(),
+//     ],
+//     server: {
+//         cors: true,
+//     },
+// });
